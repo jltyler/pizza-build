fcd8446d6b348c0128d5279d2d45edd8bffdbae9
Sumry <john.lucas.tyler@gmail.com>
2018-09-10 14:53:20 -0600

    Basic App-level structure

--- src/App.js
+++ src/App.js
@@ -2,12 +2,29 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Header from './comp/header/Header';
+import Builder from './comp/builder/Builder';
 
 class App extends Component {
+  state = {
+    display: 'builder',
+  }
+
+  getCurrentMainPage = () => {
+    switch(this.state.display) {
+      case 'builder':
+        return <Builder></Builder>
+      case 'history':
+        return <div>GOIN DOWN IN HISTORY</div>
+      default:
+        return <div className="invalid-state">INVALID STATE</div>
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <Header></Header>
+        { this.getCurrentMainPage() }
       </div>
     );
   }
---
+++ src/comp/builder/Builder.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Builder extends Component {
+    state = {}
+
+    render() {
+        return (
+            <div className="builder">
+            BUIDLER
+            </div>
+        )
+    }
+}
+
+export default Builder;
