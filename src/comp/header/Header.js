4a072a22a47aedf3562c12fc74a063027a1a11ee
Sumry <john.lucas.tyler@gmail.com>
2018-09-10 14:35:16 -0600

    Basic header structure

---
+++ src/comp/header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+const Header = (props) => {
+    return (
+        <div className="header">
+            <div className="link">Builder</div>
+            <div className="link">History</div>
+        </div>
+    )
+}
+
+export default Header
