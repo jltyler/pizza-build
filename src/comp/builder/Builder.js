4aa180c87a0b40ff8eb96299e1c3da15d372dbb7
Sumry <john.lucas.tyler@gmail.com>
2018-09-13 14:02:07 -0600

    Added price calculation to bottom controls
    Changed ingredient_list to ingredient_prices object
    Added size control dropdown menu
    Styled some things to make them easier to read

--- src/comp/builder/Builder.js
+++ src/comp/builder/Builder.js
@@ -4,29 +4,19 @@ import Controls from './controls/Controls';
 import Control from './controls/Control';
 import './Builder.css';
 
-const ingredient_list = [
-    {
-        name: 'pepperoni',
-        price: 0.5,
-    },
-    {
-    name: 'mushrooms',
-        price: 0.725,
-    },
-    {
-        name: 'black olives',
-        price: 0.65,
-    },
-    {
-        name: 'anchovies',
-        price: 1.5,
-    },
-]
+const base_price = 8.0;
+
+const ingredient_prices = {
+    'pepperoni': 0.5,
+    'mushrooms': 0.725,
+    'black olives': 0.65,
+    'anchovies': 1.5,
+}
 
 const size_table = {
     small: {
         diameter: '8 inches',
-        multiplier: 1,
+        multiplier: 1.0,
     },
     medium: {
         diameter: '10 inches',
@@ -43,17 +33,21 @@ const size_table = {
 }
 
 class Builder extends Component {
-    state = {
-        size: 'small',
-        ingredients: {},
-    }
-
     constructor(props) {
         super(props);
-        ingredient_list.forEach(i => {
-            this.state.ingredients[i.name] = 0
-        })
-        console.log('this.state.ingredients: ', this.state.ingredients)
+
+        // Assigning state once in the constructor is okay but mutating it is not
+        const temp_ingredients = {};
+        for (const i in ingredient_prices)
+        {
+            temp_ingredients[i] = 0;
+        }
+
+        this.state = {
+            size: 'small',
+            ingredients: temp_ingredients,
+        }
+        // console.log('this.state.ingredients: ', this.state.ingredients)
     }
 
     setIngredient = (ingredient, quantity) => {
@@ -87,14 +81,40 @@ class Builder extends Component {
         return ingredients;
     }
 
+    calculateTotalPrice = () => {
+        let price = base_price;
+        for (const i in this.state.ingredients) {
+            price += this.state.ingredients[i] * ingredient_prices[i];
+        }
+        return price * size_table[this.state.size].multiplier;
+    }
+
+    changeSize = (e) => {
+        // console.log('e.target.value:', e.target.value)
+        this.setState({
+            size: e.target.value,
+        })
+    }
+
     render() {
         return (
             <div className="builder">
-            <Controls>
-                {this.renderIngredients()}
-            </Controls>
-            <Preview ingredients={this.state.ingredients}/>
-            <button className="checkout">Checkout</button>
+                <Controls>
+                    Size:
+                    <select onChange={this.changeSize}>
+                        <option value="small">Small</option>
+                        <option value="medium">Medium</option>
+                        <option value="large">Large</option>
+                        <option value="party">Party</option>
+                    </select>
+                    {this.renderIngredients()}
+                </Controls>
+                <Preview ingredients={this.state.ingredients}/>
+                <div className="total">
+                    Size: {this.state.size} <br />
+                    Total: {this.calculateTotalPrice().toFixed(2)}
+                </div>
+                <button className="checkout">Checkout</button>
             </div>
         )
     }
--- src/comp/builder/controls/Control.js
+++ src/comp/builder/controls/Control.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Control = (props) => {
     return (
         <div className="control">
-        <span>{props.name}</span>
+            <span>{props.name}</span>
             <button onClick={props.removeFunc}>No</button>
             <button onClick={props.addFunc}>Yes</button>
             <button onClick={props.doubleFunc}>Double</button>
