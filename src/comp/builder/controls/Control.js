d17538cd5988fd6794839903bdc5e49ca47c640c
Sumry <john.lucas.tyler@gmail.com>
2018-09-26 14:49:11 -0600

    Disabled buttons for selected choice and styled em

--- src/comp/builder/controls/Control.js
+++ src/comp/builder/controls/Control.js
@@ -4,9 +4,9 @@ const Control = (props) => {
     return (
         <div className="control">
             <span>{props.name}</span>
-            <button onClick={props.removeFunc}>No</button>
-            <button onClick={props.addFunc}>Yes</button>
-            <button onClick={props.doubleFunc}>Double</button>
+            <button onClick={props.removeFunc} disabled={props.current === 0}>None</button>
+            <button onClick={props.addFunc} disabled={props.current === 1}>Some</button>
+            <button onClick={props.doubleFunc} disabled={props.current === 2}>Lots</button>
         </div>
     )
 }
