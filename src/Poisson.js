aad73e8b390b4e7113cf501abbfe6a0d6eac55b3
Sumry <john.lucas.tyler@gmail.com>
2018-10-16 16:18:05 -0600

    Extended and moved Poisson to it's own module
    Cleaned up functions a bit
    Documented functions for Poisson

---
+++ src/Poisson.js
@@ -0,0 +1,151 @@
+import Helpers from './Helpers';
+
+const defaultOptions = {
+    number: 0,
+    attempts: 30,
+    cullDistance: 0,
+}
+
+/**
+ * Generates and returns a list of coordinate pairs using Poisson-Disc sampling algorithm
+ * @arg {number} width - Width of field to generate points in
+ * @arg {number} height - Height of field to generate points in
+ * @arg {number} minDistance - Minimum distance between points
+ * @arg {Object} [options] - Other options for generation
+ * @arg {number} options.number - Number of points to generate. Attempts to fill space if this is 0
+ * @arg {number} options.attempts - Number of attempts to generate a point before giving up on that point. Defaults to 30
+ * @arg {number} options.cullDistance - Any points further than this from the center of the field are removed from the final sample list
+ * 
+ * @returns {Array} - Array of generated coordinate pairs
+*/
+
+const generateSamples = (width, height, minDistance, options = {}) => {
+    options = {...defaultOptions, ...options};
+
+    // Some precalculations
+    const cullDistance2 = Math.pow(options.cullDistance, 2);
+    const minDistance2 = Math.pow(minDistance, 2);
+    const center = [width / 2, height / 2];
+
+    // Diagonal cell length must be less than minimum distance to ensure a maximum of one point per cell
+    const cellSize = Math.floor(minDistance * 0.7071067811);
+    const gridWidth = Math.floor(width / cellSize) + 1;
+    const gridHeight = Math.floor(height / cellSize) + 1;
+    const grid = Array(gridWidth);
+    for(let i = 0; i < gridWidth; ++i)
+    {
+        grid[i] = Array(gridHeight);
+    };
+
+    const samples = []; // Stores coordinate pairs for confirmed points
+
+    /**
+     * Takes coordinate pair and returns cell coordinates inside grid
+     * @arg {number[]} coords - Coordinate pair
+     * @returns {number[]} Grid coordinate pair
+    */
+    const gridCoords = (coords) => {
+        return [
+            Math.floor(coords[0] / cellSize),
+            Math.floor(coords[1] / cellSize),
+        ];
+    };
+
+    /**
+     * Searches neighboring cells and returns a list of indexes of neighboring points
+     * @param {number[]} coords Grid coordinate pair to check around
+     * @returns {number[]} Indexes of neighboring points
+     */
+    const getNeighbors = (coords) => {
+        const relativePositions = [
+            [-1, -2], [0, -2], [1, -2],
+            [-2, -1], [-1, -1], [0, -1], [1, -1], [2, -1],
+            [-2, 0], [-1, 0], [0, 0], [1, 0], [2, 0],
+            [-2, 1], [-1, 1], [0, 1], [1, 1], [2, 1],
+            [-1, 2], [0, 2], [1, 2],
+        ];
+        const neighbors = [];
+        for(let i = 0; i < relativePositions.length; ++i)
+        {
+            const nx = coords[0] + relativePositions[i][0];
+            const ny = coords[1] + relativePositions[i][1];
+            // If the cell is out of bounds or empty we skip forward
+            if (nx < 0 || nx >= gridWidth || ny < 0 || ny >= gridHeight || grid[nx][ny] === undefined) continue;
+            neighbors.push(grid[nx][ny]);
+        };
+        return neighbors;
+    }
+
+    /**
+     * Checks for any existing points around a new point
+     * @param {number[]} coords Coordinate pair
+     * @returns {boolean} True if no points are nearby, false otherwise
+     */
+    const checkDistance = (coords) => {
+        
+        const gc = gridCoords(coords);
+        const neighbors = getNeighbors(gc);
+        for (let i = 0; i < neighbors.length; i++) {
+            const compareCoords = samples[neighbors[i]];
+            const distance = Math.pow(coords[0] - compareCoords[0], 2) + Math.pow(coords[1] - compareCoords[1], 2);
+            
+            if (distance <= minDistance2)
+            {
+                return false;
+            }
+        }
+        
+        return true;
+    };
+
+    /**
+     * Attempts to generate a new point using a reference point to check around
+     * @param {number[]} referenceCoords Coordinate pair
+     * @returns {(number[]|undefined)} The new coordinate pair if successful, undefined if ran out of attempts
+     */
+    const findPoint = (referenceCoords) => {
+        for(let i = options.attempts; i > 0; --i)
+        {
+            const dist = Math.random() * minDistance + minDistance;
+            const dir = Math.random() * Math.PI * 2;
+            const newPoint = [
+                referenceCoords[0] + Math.cos(dir) * dist,
+                referenceCoords[1] + Math.sin(dir) * dist,
+            ];
+            if (newPoint[0] < 0 || newPoint[0] >= width || newPoint[1] < 0 || newPoint[1] >= height) continue;
+            if (checkDistance(newPoint)) return newPoint;
+        }
+        
+        return undefined;
+    }
+
+    // Start of actual generation
+    const startPoint = [Math.random() * width, Math.random() * height];
+    samples.push(startPoint);
+    const activePoints = [0];
+    // First point grid location
+    const startCell = gridCoords(startPoint);
+    grid[startCell[0]][startCell[1]] = 0;
+    
+    while(activePoints.length > 0 && samples.length !== options.number) {  
+        const refIndex = Helpers.choose(activePoints);
+        const refCoords = samples[refIndex];
+        const newPoint = findPoint(refCoords);
+        if (newPoint !== undefined)
+        {
+            const index = samples.length;
+            samples.push(newPoint);
+            activePoints.push(index);
+            const newCell = gridCoords(newPoint);
+            grid[newCell[0]][newCell[1]] = index;
+        }
+        else
+        {
+            activePoints.splice(activePoints.indexOf(refIndex), 1);
+        }
+    }
+
+    return (cullDistance2 <= 0) ? samples : samples.filter(s => (Math.pow(s[0] - center[0], 2) + Math.pow(s[1] - center[1], 2) < cullDistance2));
+};
+
+export default generateSamples;
--- src/comp/builder/preview/Preview.js
+++ src/comp/builder/preview/Preview.js
@@ -1,110 +1,9 @@
 import React from 'react';
 import './Preview.css'
 import Helpers from '../../../Helpers';
+import Poisson from '../../../Poisson';
 
-const width = 400;
-const height = 400;
-const cull_center_distance = Math.pow(180, 2); // Distance (squared) of culling edge
-const min_distance = 40; // Minimum distance between points
-const min_distance2 = min_distance * min_distance;
-const attempts = 20; // Max attempts at generating a new point before giving up
-
-const cell_size = min_distance / 1.41421;
-const grid_width = Math.floor(width / cell_size) + 1;
-const grid_height = Math.floor(height / cell_size) + 1;
-const grid = Array(grid_width);
-for(let i = 0; i < grid_width; ++i)
-{
-    grid[i] = Array(grid_height);
-};
-
-const samples = [];
-const active_points = [];
-
-const grid_coords = (coords) => {
-    return [
-        Math.floor(coords[0] / cell_size),
-        Math.floor(coords[1] / cell_size),
-    ];
-};
-
-const get_neighbors = (coords) => {
-    const relative_positions = [
-        [-1, -2], [0, -2], [1, -2],
-        [-2, -1], [-1, -1], [0, -1], [1, -1], [2, -1],
-        [-2, 0], [-1, 0], [0, 0], [1, 0], [2, 0],
-        [-2, 1], [-1, 1], [0, 1], [1, 1], [2, 1],
-        [-1, 2], [0, 2], [1, 2],
-    ];
-    const neighbors = [];
-    for(let i = 0; i < relative_positions.length; ++i)
-    {
-        const nx = coords[0] + relative_positions[i][0];
-        const ny = coords[1] + relative_positions[i][1];
-        if (nx < 0 || nx >= grid_width || ny < 0 || ny >= grid_height || grid[nx][ny] === undefined) continue;
-        neighbors.push(grid[nx][ny]);
-    };
-    return neighbors;
-}
-
-const check_distance = (coords) => {
-    const gc = grid_coords(coords);
-    const neighbors = get_neighbors(gc);
-    for (let i = 0; i < neighbors.length; i++) {
-        const compare_coords = samples[neighbors[i]];
-        const distance = Math.pow(coords[0] - compare_coords[0], 2) + Math.pow(coords[1] - compare_coords[1], 2);
-        if (distance <= min_distance2) return false;
-    }
-    return true;
-};
-
-const find_point = (reference_coords) => {
-    for(let i = attempts; i > 0; --i)
-    {
-        const dist = Math.random() * min_distance + min_distance;
-        const dir = Math.random() * Math.PI * 2;
-        const new_point = [
-            reference_coords[0] + Math.cos(dir) * dist,
-            reference_coords[1] + Math.sin(dir) * dist,
-        ];
-        if (new_point[0] < 0 || new_point[0] >= width || new_point[1] < 0 || new_point[1] >= height) continue;
-        if (check_distance(new_point)) return new_point;
-    }
-    return undefined;
-}
-
-// Start of actual generation
-const start_point = [width / 2, height / 2];
-samples.push(start_point);
-// First point grid location
-const start_cell = grid_coords(start_point);
-grid[start_cell[0]][start_cell[1]] = 0;
-active_points.push(0);
-
-while(active_points.length > 0) {
-    const ref_index = Helpers.choose(active_points);
-    const ref_coords = samples[ref_index];
-    const new_point = find_point(ref_coords);
-    if (new_point !== undefined)
-    {
-        const index = samples.length;
-        samples.push(new_point);
-        active_points.push(index);
-        const new_cell = grid_coords(new_point);
-        grid[new_cell[0]][new_cell[1]] = index;
-    }
-    else
-    {
-        active_points.splice(active_points.indexOf(ref_index), 1); 
-    }
-}
-
-const positions = samples.filter(s => {
-    const dx = s[0] - width / 2;
-    const dy = s[1] - height / 2;
-    if (dx * dx + dy * dy > cull_center_distance) return false;
-    return true;
-})
+const positions = Poisson(400, 400, 40, {cullDistance: 180});
 
 const ListIngredients = (ingredients) => {
     const ret_list = [];
@@ -130,7 +29,7 @@ const Preview = (props) => {
                 })}
 
             </div>
-            {/* {ListIngredients(props.ingredients)} */}
+            {ListIngredients(props.ingredients)}
         </div>
     )
 }
