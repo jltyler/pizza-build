78e700854de440909115a9cc9b4f615bf29d73c8
Sumry <john.lucas.tyler@gmail.com>
2018-10-16 17:18:34 -0600

    Added olives to preview

--- src/comp/builder/preview/Preview.js
+++ src/comp/builder/preview/Preview.js
@@ -5,19 +5,23 @@ import Poisson from '../../../Poisson';
 
 const pizzaSize = 400 - 16 * 2;
 
-const positions = Poisson(pizzaSize, pizzaSize, 30, {cullDistance: 180});
-
 const ingredientMapper = (name, p, i) => {
     return <div
         className={name}
-        style={{left: p[0] - 10, top: p[1] - 10}}
+        style={{left: p[0] - 10, top: p[1] - 10, transform: 'rotate(' + (Math.random() * 360).toFixed(0) + 'deg)'}}
         key={i} />
 }
 
 const pepperoni_elements = [
     null,
-    Poisson(pizzaSize, pizzaSize, 35, {cullDistance: 180}).map(ingredientMapper.bind(null, 'pepperoni')),
-    Poisson(pizzaSize, pizzaSize, 25, {cullDistance: 180}).map(ingredientMapper.bind(null, 'pepperoni')),
+    Poisson(pizzaSize, pizzaSize, 35, {cullDistance: 185}).map(ingredientMapper.bind(null, 'pepperoni')),
+    Poisson(pizzaSize, pizzaSize, 23, {cullDistance: 185}).map(ingredientMapper.bind(null, 'pepperoni')),
+]
+
+const olive_elements = [
+    null,
+    Poisson(pizzaSize, pizzaSize, 25, {cullDistance: 173}).map(ingredientMapper.bind(null, 'olive')),
+    Poisson(pizzaSize, pizzaSize, 14, {cullDistance: 173}).map(ingredientMapper.bind(null, 'olive')),
 ]
 
 const ListIngredients = (ingredients) => {
@@ -34,6 +38,7 @@ const Preview = (props) => {
         <div className="preview">
             <div className="pizza-base">
                 {pepperoni_elements[props.ingredients.pepperoni]}
+                {olive_elements[props.ingredients['black olives']]}
 
             </div>
             {ListIngredients(props.ingredients)}
