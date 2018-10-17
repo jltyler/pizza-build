d1d7430f04f09cff7c0ca27c6c014e7cddf639f7
John Tyler <john.lucas.tyler@gmail.com>
2018-10-17 12:04:56 -0600

    Added mushrooms
    Fixed some styling

--- src/comp/builder/preview/Preview.js
+++ src/comp/builder/preview/Preview.js
@@ -6,19 +6,35 @@ import Poisson from '../../../Poisson';
 const pizzaSize = 400 - 16 * 2;
 
 const ingredientMapper = (name, p, i) => {
-    return <div
+    return (<div
         className={name}
         style={{left: p[0] - 10, top: p[1] - 10, transform: 'rotate(' + (Math.random() * 360).toFixed(0) + 'deg)'}}
-        key={i} />
+        key={name + i} />)
 }
 
-const pepperoni_elements = [
+const pepperoniElements = [
     null,
     Poisson(pizzaSize, pizzaSize, 35, {cullDistance: 185}).map(ingredientMapper.bind(null, 'pepperoni')),
     Poisson(pizzaSize, pizzaSize, 23, {cullDistance: 185}).map(ingredientMapper.bind(null, 'pepperoni')),
 ]
 
-const olive_elements = [
+const mushroomMapper = (p, i) => {
+    return (<div
+        className="mushroom"
+        style={{left: p[0] - 10, top: p[1] - 10, transform: 'rotate(' + (Math.random() * 360).toFixed(0) + 'deg)'}}
+        key={"mushroom" + i}>
+            <div className="mushroom-stem" />
+            <div className="mushroom-head"></div>
+        </div>)
+}
+
+const mushroomElements = [
+    null,
+    Poisson(pizzaSize, pizzaSize, 35, {cullDistance: 173}).map(mushroomMapper),
+    Poisson(pizzaSize, pizzaSize, 25, {cullDistance: 173}).map(mushroomMapper),
+]
+
+const oliveElements = [
     null,
     Poisson(pizzaSize, pizzaSize, 25, {cullDistance: 173}).map(ingredientMapper.bind(null, 'olive')),
     Poisson(pizzaSize, pizzaSize, 14, {cullDistance: 173}).map(ingredientMapper.bind(null, 'olive')),
@@ -37,9 +53,10 @@ const Preview = (props) => {
     return (
         <div className="preview">
             <div className="pizza-base">
-                {pepperoni_elements[props.ingredients.pepperoni]}
-                {olive_elements[props.ingredients['black olives']]}
-
+                {pepperoniElements[props.ingredients.pepperoni]}
+                {oliveElements[props.ingredients['black olives']]}
+                {mushroomElements[props.ingredients.mushrooms]}
+                
             </div>
             {ListIngredients(props.ingredients)}
         </div>
