013f1fbd33ef3b7a87f4944e3be1156ac1b42271
John Tyler <john.lucas.tyler@gmail.com>
2018-10-17 13:47:42 -0600

    Added pineapple, green chiles, and anchovies

--- src/comp/builder/preview/Preview.js
+++ src/comp/builder/preview/Preview.js
@@ -24,20 +24,38 @@ const mushroomMapper = (p, i) => {
         style={{left: p[0] - 10, top: p[1] - 10, transform: 'rotate(' + (Math.random() * 360).toFixed(0) + 'deg)'}}
         key={"mushroom" + i}>
             <div className="mushroom-stem" />
-            <div className="mushroom-head"></div>
+            <div className="mushroom-head" />
         </div>)
 }
 
 const mushroomElements = [
     null,
-    Poisson(pizzaSize, pizzaSize, 35, {cullDistance: 173}).map(mushroomMapper),
-    Poisson(pizzaSize, pizzaSize, 25, {cullDistance: 173}).map(mushroomMapper),
+    Poisson(pizzaSize, pizzaSize, 35, {cullDistance: 170}).map(mushroomMapper),
+    Poisson(pizzaSize, pizzaSize, 25, {cullDistance: 170}).map(mushroomMapper),
 ]
 
 const oliveElements = [
     null,
     Poisson(pizzaSize, pizzaSize, 25, {cullDistance: 173}).map(ingredientMapper.bind(null, 'olive')),
     Poisson(pizzaSize, pizzaSize, 14, {cullDistance: 173}).map(ingredientMapper.bind(null, 'olive')),
+]
+
+const pineappleElements = [
+    null,
+    Poisson(pizzaSize, pizzaSize, 30, {cullDistance: 170}).map(ingredientMapper.bind(null, 'pineapple')),
+    Poisson(pizzaSize, pizzaSize, 20, {cullDistance: 170}).map(ingredientMapper.bind(null, 'pineapple')),
+]
+
+const greenchileElements = [
+    null,
+    Poisson(pizzaSize + 4, pizzaSize + 4, 20, {cullDistance: 175}).map(ingredientMapper.bind(null, 'greenchile')),
+    Poisson(pizzaSize + 4, pizzaSize + 4, 10, {cullDistance: 175}).map(ingredientMapper.bind(null, 'greenchile')),
+]
+
+const anchovyElements = [
+    null,
+    Poisson(pizzaSize, pizzaSize, 55, {cullDistance: 175}).map(ingredientMapper.bind(null, 'anchovy')),
+    Poisson(pizzaSize, pizzaSize, 40, {cullDistance: 175}).map(ingredientMapper.bind(null, 'anchovy')),
 ]
 
 const ListIngredients = (ingredients) => {
@@ -56,6 +74,9 @@ const Preview = (props) => {
                 {pepperoniElements[props.ingredients.pepperoni]}
                 {oliveElements[props.ingredients['black olives']]}
                 {mushroomElements[props.ingredients.mushrooms]}
+                {pineappleElements[props.ingredients.pineapple]}
+                {greenchileElements[props.ingredients['green chile']]}
+                {anchovyElements[props.ingredients['anchovies']]}
                 
             </div>
             {ListIngredients(props.ingredients)}
